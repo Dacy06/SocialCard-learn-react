935166df8ef455c09007ca9d3a764566eeb9a60c
zhangwenjie16 <zhangwenjie16@meituan.com>
2020-07-22 20:37:26 +0800

    Updates

---
+++ precache-manifest.3a11ad96298c0c9e4ef1f796b4b69163.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3d889c6c11a907936acd8253c3e9bb",
+    "url": "/SocialCard-learn-react/index.html"
+  },
+  {
+    "revision": "9fb44dedba79c4c963cf",
+    "url": "/SocialCard-learn-react/static/css/main.357f0058.chunk.css"
+  },
+  {
+    "revision": "a480709a3d350f9b5e01",
+    "url": "/SocialCard-learn-react/static/js/2.22f2817e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SocialCard-learn-react/static/js/2.22f2817e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb44dedba79c4c963cf",
+    "url": "/SocialCard-learn-react/static/js/main.b299082d.chunk.js"
+  },
+  {
+    "revision": "167ad6307a9d76a72a3c",
+    "url": "/SocialCard-learn-react/static/js/runtime-main.c4daf6a5.js"
+  },
+  {
+    "revision": "b7d02eb8681b62ce93bf61be252d1d9e",
+    "url": "/SocialCard-learn-react/static/media/1249256.b7d02eb8.png"
+  },
+  {
+    "revision": "1d7d74d77701bbe8cffeb20b8e69253d",
+    "url": "/SocialCard-learn-react/static/media/forward.1d7d74d7.png"
+  }
+]);
